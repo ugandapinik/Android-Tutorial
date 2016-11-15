c94683324d9b62f764abb37a367b63de441b88c8
wohhie Mahmud <wohhie Mahmud>
2016-11-15 14:04:01 +0600

    Uploaded the android Wordpress Blog

---
+++ AndroidWordpressBlog/app/src/main/java/com/example/wohhie/androidwordpressblog/Posts.java
@@ -0,0 +1,73 @@
+package com.example.wohhie.androidwordpressblog;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+
+public class Posts extends AppCompatActivity {
+    TextView title;
+    WebView content;
+    ProgressDialog progressDialog;
+    Gson gson;
+    Map<String, Object> mapPost;
+    Map<String, Object> mapTitle;
+    Map<String, Object> mapContent;
+
+    String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+
+        final String id = getIntent().getExtras().getString("id");
+
+        title = (TextView) findViewById(R.id.title);
+        content = (WebView)findViewById(R.id.content);
+
+        progressDialog = new ProgressDialog(Posts.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+        String url = "http://www.jewel-mahmud.com/wp-json/wp/v2/posts/"+id+"?fields=title,content";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                gson = new Gson();
+                mapPost = (Map<String, Object>) gson.fromJson(s, Map.class);
+                mapTitle = (Map<String, Object>) mapPost.get("title");
+                mapContent = (Map<String, Object>) mapPost.get("content");
+
+                title.setText(mapTitle.get("rendered").toString());
+                content.loadData(mapContent.get("rendered").toString(),"text/html","UTF-8");
+
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                progressDialog.dismiss();
+                Toast.makeText(Posts.this, id, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue rQueue = Volley.newRequestQueue(Posts.this);
+        rQueue.add(request);
+    }
+}
