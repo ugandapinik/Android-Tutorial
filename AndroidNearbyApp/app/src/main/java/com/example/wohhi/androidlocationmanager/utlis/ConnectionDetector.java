67af654c45ea18c887ea7349e076437c61d72915
wohhie Mahmud <wohhie Mahmud>
2017-06-13 01:34:48 +0600

    Nearby Places display Added

---
+++ AndroidNearbyApp/app/src/main/java/com/example/wohhi/androidlocationmanager/utlis/ConnectionDetector.java
@@ -0,0 +1,21 @@
+package com.example.wohhi.androidlocationmanager.utlis;
+
+import android.content.Context;
+
+/**
+ * Created by wohhi on 6/13/2017.
+ */
+
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context context){
+        this._context = context;
+    }
+
+    /**
+     * Checking for all possible internet providers
+     * **/
+
+}
